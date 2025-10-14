85163a4892a3b6b275746274b312feb88cffd057
SidiJr <sednei.jr@gmail.com>
2025-10-13 23:43:07 -0300

    feat: :sparkles: Inciando login e cors
    Começando login e configurando cors.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log('App iniciado com sucesso'))
+  .catch((err) => {
+    console.error('Erro ao iniciar o app:', err);
+    process.exit(1);
+  });
