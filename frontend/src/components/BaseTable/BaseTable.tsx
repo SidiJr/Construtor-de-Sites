bda7afa269724f41e1b7dbecde11f4e28989b7a0
SidiJr <sednei.jr@gmail.com>
2025-10-22 23:28:59 -0300

    feat: :sparkles: Edit e Delete na tabela de listagem
    Edit e Delete na tabela de listagem.

--- frontend/src/components/BaseTable/BaseTable.tsx
+++ frontend/src/components/BaseTable/BaseTable.tsx
@@ -9,8 +9,11 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { api } from "@/lib/api";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Edit, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 type BaseTableProps = {
   route: string;
@@ -25,6 +28,7 @@ export default function BaseTable({
 }: BaseTableProps) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [data, setData] = useState<Record<string, any>[] | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     const handleGetData = async () => {
@@ -50,6 +54,38 @@ export default function BaseTable({
     handleGetData();
   }, [route]);
 
+  const handleEdit = useCallback(
+    async (id: string | number) => {
+      router.push(`/${route}s/${id}`);
+    },
+    [router, route]
+  );
+
+  const handleDelete = useCallback(
+    async (id: string | number) => {
+      console.log("Excluir id:", id);
+      try {
+        const res = await fetch(`${api}/${route}/${id}`, {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+        });
+        const result = await res.json();
+
+        if (result.status === "error") {
+          toast.error(result.message);
+        } else {
+          toast.success(result.message);
+          setData(
+            (prevData) => prevData?.filter((item) => item.id !== id) || []
+          );
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [route]
+  );
+
   return (
     <Table>
       <TableHeader>
@@ -59,19 +95,25 @@ export default function BaseTable({
               {column.charAt(0).toUpperCase() + column.slice(1)}
             </TableHead>
           ))}
-         <TableHead>Ações</TableHead>
+          <TableHead>Ações</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {data === null ? (
           <TableRow>
-            <TableCell colSpan={columns.length} className="text-center py-4">
+            <TableCell
+              colSpan={columns.length + 1}
+              className="text-center py-4"
+            >
               Carregando...
             </TableCell>
           </TableRow>
         ) : data.length === 0 ? (
           <TableRow>
-            <TableCell colSpan={columns.length} className="text-center py-4">
+            <TableCell
+              colSpan={columns.length + 1}
+              className="text-center py-4"
+            >
               Nenhum dado encontrado.
             </TableCell>
           </TableRow>
@@ -80,12 +122,32 @@ export default function BaseTable({
             <TableRow key={item.id}>
               {columns.map((col) => {
                 const value = item[col];
-                const mask = customMask[col]; //Verifica se tem máscara pra essa coluna
+                const mask = customMask[col];
                 const displayValue =
                   mask && mask[value] ? mask[value] : String(value);
                 return <TableCell key={col}>{displayValue}</TableCell>;
               })}
-              <TableCell></TableCell>
+              <TableCell className="flex gap-2">
+                {/* Botão de Editar */}
+                <Button
+                  variant="default"
+                  size="sm"
+                  className="w-8 h-8 p-0 flex items-center justify-center bg-blue-500 text-white hover:bg-blue-600"
+                  onClick={() => handleEdit(item.id)}
+                >
+                  <Edit className="w-4 h-4" />
+                </Button>
+
+                {/* Botão de Excluir */}
+                <Button
+                  variant="default"
+                  size="sm"
+                  className="w-8 h-8 p-0 flex items-center justify-center bg-red-500 text-white hover:bg-red-600"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  <Trash2 className="w-4 h-4" />
+                </Button>
+              </TableCell>
             </TableRow>
           ))
         )}
